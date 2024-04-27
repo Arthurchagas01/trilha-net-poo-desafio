c9744d3fa023e964e077fc720d8553163a20105b
Arthur <arthurchagas01@gmail.com>
2024-04-27 13:29:17 -0300

    Programa criado e testado!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("952979001", "N95", "112023400199", "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Pokemon Go");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("952979001", "15 pro", "458789963267", "128");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("YouTube");
